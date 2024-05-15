c8726ca75d40edd0b9386e45495e5a5eeb7c235c
Anil <kanilreddy867@gmail.com>
2024-05-15 23:58:09 +0530

    trying out router methods.

--- vue_frontend/src/main.js
+++ vue_frontend/src/main.js
@@ -1,5 +1,10 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter } from "vue-router";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const router = createRouter({});
+
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
