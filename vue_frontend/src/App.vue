442d596de969145671f4e4d34a82a8f594d355da
Anil <kanilreddy867@gmail.com>
2024-05-17 02:51:05 +0530

    added footer from a codepen by Anup, adjusted as per my application.

--- vue_frontend/src/App.vue
+++ vue_frontend/src/App.vue
@@ -1,12 +1,14 @@
 <script setup>
 import router from './main';
 import NavBar from "./components/Nav.vue";
+import Footer from "./components/Footer.vue"
 </script>
 
 <template>
   <div></div>
   <NavBar />
   <router-view />
+  <Footer />
 </template>
 
 <style scoped></style>
